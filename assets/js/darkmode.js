76cf02a23e35bbea577541e39e6f1ec3fb4656ea
Fredrik Johansen <fredrik1206@gmail.com>
2021-12-08 10:35:08 +0100

    Fixed settings menu

--- assets/js/darkmode.js
+++ assets/js/darkmode.js
@@ -28,12 +28,19 @@ function setTheme(isDarkMode) {
 
 function toggleDarkMode() {
 	const result = checkSetting();
+	const button = document.getElementById("darkModeButton");
 
 	if (result == "true") {
 		localStorage.setItem("isDarkMode", "false");
 		setTheme("false");
+		button.innerText = "Turn On";
+		button.classList.remove("btn-danger");
+		button.classList.add("btn-success");
 	} else {
 		localStorage.setItem("isDarkMode", "true");
 		setTheme("true");
+		button.innerText = "Turn Off";
+		button.classList.remove("btn-success");
+		button.classList.add("btn-danger");
 	}
 }
